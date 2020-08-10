3d46b32d845aa29bbd7cd41ce4052fc1360138fa
malaoun <mohaimin.alaoun@clarabridge.com>
2020-08-10 12:07:22 -0400

    updates

--- lerna_demo/packages/mohaimin-skeleton/build.js
+++ lerna_demo/packages/mohaimin-skeleton/build.js
@@ -4,7 +4,7 @@ var fs = require('fs'),
     htmlString = fs.readFileSync('./templates/starter.html', 'utf-8'),
     iconsDest = '';
 
-//console.log('\x1b[36m%s\x1b[0m', 'I am cyan', 'hello world'); //cyan
+console.log('\x1b[36m%s\x1b[0m', 'I am cyan', 'hello world'); //cyan
 
 var iconsDest = fs.existsSync('../../src') ?  '../../src/cb-icons' : './cb-icons';
 if (!fs.existsSync(iconsDest)) fs.mkdirSync(iconsDest);
@@ -13,24 +13,24 @@ if (!fs.existsSync(iconsDest)) fs.mkdirSync(iconsDest);
 fs.createReadStream('./styles/dog.png')
     .pipe(fs.createWriteStream(iconsDest+'/dog.png'));
 
-//
-//
-// if (fs.existsSync('../../src')) {
-//     fs.writeFile('../../src/starter-template.html', htmlString, function (err) {
-//         if (err) throw err;
-//         console.log('\x1b[36m%s\x1b[0m','Template is created in src folder successfully.');
-//     });
-//     fs.writeFile('../../src/style.css', styleString, function (err) {
-//         if (err) throw err;
-//         console.log('File is created in src folder successfully.');
-//     });
-// } else {
-//     fs.writeFile('../../starter-template.html', htmlString, function (err) {
-//         if (err) throw err;
-//         console.log('\x1b[36m%s\x1b[0m','Template created in root folder');
-//     });
-//     fs.writeFile('../../style.css', styleString, function (err) {
-//         if (err) throw err;
-//         console.log('File is created in root folder.');
-//     });
-// }
+
+
+if (fs.existsSync('../../src')) {
+    fs.writeFile('../../src/starter-template.html', htmlString, function (err) {
+        if (err) throw err;
+        console.log('\x1b[36m%s\x1b[0m','Template is created in src folder successfully.');
+    });
+    fs.writeFile('../../src/style.css', styleString, function (err) {
+        if (err) throw err;
+        console.log('File is created in src folder successfully.');
+    });
+} else {
+    fs.writeFile('../../starter-template.html', htmlString, function (err) {
+        if (err) throw err;
+        console.log('\x1b[36m%s\x1b[0m','Template created in root folder');
+    });
+    fs.writeFile('../../style.css', styleString, function (err) {
+        if (err) throw err;
+        console.log('File is created in root folder.');
+    });
+}
---
+++ lerna_tester/index.js
@@ -0,0 +1,2 @@
+var common = require('mohaimin_lerna_demo/packages/common');
+console.log(common.add(3,4));
